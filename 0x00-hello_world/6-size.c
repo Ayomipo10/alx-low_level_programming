38d44f726d4c9bcfef9dff66f32b62c72d1558f9
Ayomipo10 <tizwilj@gmail.com>
2022-12-08 20:10:45 -0800

    New solutions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,11 @@ int main(void)
 
 		size = 4;
 	printf("Size of an int:%d bytes(s)\n", size);
-		size = 8;
-		printf("Size of a long int:%d byte(s)\n", size);
-		size = 8;
-		printf("Size of a long long int:%d byte(s)\n", size);
-		size = 4;
+	size = 4;
 		printf("Size of a float:%d byte(s)\n", size);
+
+		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 		return (0);
 }
 
