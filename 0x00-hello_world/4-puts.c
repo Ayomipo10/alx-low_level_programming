3125cd10b70901905e0369006b024d971ca7a9be
Ayomipo10 <tizwilj@gmail.com>
2022-12-08 18:44:40 -0800

    Editted solutions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/*Using the puts function to print a statement*/
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/*Using the printf function to print a statement*/
-
-int main(void)
+/**
+ * main -Prints out the phrase "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+	int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
