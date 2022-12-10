592e08bfc9b3394157f9b576db9030b4941719d7
Ayomipo10 <tizwilj@gmail.com>
2022-12-09 20:43:57 -0800

    New solutions

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,22 +2,22 @@
 
 /**
  * main - prints all possible different combinations of two digits
- * Return - Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int n, m;
+	int x, y;
 
-	for (n = 48; n <= 56; n++)
+	for (x = 48; x <= 56; x++)
 	{
-		for (m = 49; m <= 57; m++)
+		for (y = 49; y <= 57; y++)
 		{
-			if (m > n)
+			if (x > y)
 			{
-				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
 				{
 					putchar(',');
 					putchar(' ');
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,25 +2,25 @@
 
 /**
  * main - prints all possible different combinations of three digits
- * Return - Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int n, m, l;
+	int x, y, z;
 
-	for (n = 48; n < 58; n++)
+	for (x = 48; x < 58; x++)
 	{
-		for (m = 49; m < 58; m++)
+		for (y = 49; y < 58; y++)
 		{
-			for (l = 50; l < 58; l++)
+			for (z = 50; z < 58; z++)
 			{
-				if (l > m && m > n)
+				if (z > y && y > x)
 				{
-					putchar(n);
-					putchar(m);
-					putchar(1);
-					if (n != 55 || m != 56)
+					putchar(x);
+					putchar(y);
+					putchar(z);
+					if (x != 55 || y != 56)
 					{
 						putchar(',');
 						putchar(' ');
