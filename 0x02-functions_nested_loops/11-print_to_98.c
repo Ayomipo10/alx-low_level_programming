d783c5471d7e1eec7cc32d7811bdf9498f13254a
Ayomipo10 <tizwilj@gmail.com>
2022-12-13 19:38:36 -0800

    new solutions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
+ */
+
+int add(int a, int b)
+{
+	int c = a + b;
+	return (c);
+}
